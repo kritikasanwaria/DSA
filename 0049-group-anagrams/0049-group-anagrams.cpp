d8f38b1f4a957f1069150acb9764f592be7c4d62
Kritika Sanwaria <Kritikasan2017@gmail.com>
2026-08-18 23:38:00 +0530

    Time: 81 ms (5.08%), Space: 37.4 MB (5.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,4 +1,34 @@
+//using frequency approach
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string,vector<string>>mp;
+        for (int i=0; i<strs.size(); i++){
+            vector<int> freq(26, 0);
+
+            for (int j = 0; j < strs[i].size(); j++) {
+               
+                freq[strs[i][j] - 'a']++;
+            }
+            string key = "";
+
+            for (int j = 0; j < 26; j++) {
+                key += to_string(freq[j]) + "#";
+            }
+            mp[key].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+        for (auto x : mp) {
+            ans.push_back(x.second);
+        }
+        return ans;
+}};
+
+
+
 //sorting the string & then grouping using hashmap 
+/*
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -13,7 +43,6 @@ public:
             ans.push_back(x.second);
         }
         return ans; 
-    }
-};
-
+}};
+*/
 
